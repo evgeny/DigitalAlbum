076668c57c5e595e2e4974df7e959f9c8a329e2f
Evgeny Zinovyev <e.zinovyev@gmail.com>
2018-12-03 21:28:54 +0100

    add akbum card .

---
+++ app/src/main/java/com/ezino/digitalalbum/viewmodels/AlbumListViewModel.kt
@@ -0,0 +1,8 @@
+package com.ezino.digitalalbum.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.ezino.digitalalbum.data.AlbumRepository
+
+class AlbumListViewModel internal constructor(private val albumRepository: AlbumRepository) : ViewModel() {
+
+}
