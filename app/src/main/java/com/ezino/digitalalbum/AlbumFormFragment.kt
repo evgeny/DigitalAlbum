b02a56bc6ef55f5ecccca5e02dc331b03113b45b
Evgeny Zinovyev <e.zinovyev@gmail.com>
2018-12-11 13:03:51 +0100

    add coroutine lib with first draft

--- app/src/main/java/com/ezino/digitalalbum/AlbumFormFragment.kt
+++ app/src/main/java/com/ezino/digitalalbum/AlbumFormFragment.kt
@@ -7,19 +7,46 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.ezino.digitalalbum.data.Album
 import com.ezino.digitalalbum.di.Injectors
 import com.ezino.digitalalbum.viewmodels.AlbumFormViewModel
 import kotlinx.android.synthetic.main.album_form_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
 
 class AlbumFormFragment : Fragment() {
 
+    private lateinit var viewModel: AlbumFormViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val layout = inflater.inflate(R.layout.album_form_fragment, container, false)
-        val model =
-            ViewModelProviders.of(this, activity?.application?.let { Injectors.provideAlbumFormViewModelFactory(it) })
-                .get(AlbumFormViewModel::class.java)
-        model.tile().observe(this, Observer { title -> album_title.setText(title) })
+        viewModel =
+                ViewModelProviders.of(
+                    this,
+                    activity?.application?.let { Injectors.provideAlbumFormViewModelFactory(it) })
+                    .get(AlbumFormViewModel::class.java)
+        viewModel.tile().observe(this, Observer { title -> album_title.setText(title) })
+
 
         return layout
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        create_album_btn.setOnClickListener {
+            GlobalScope.launch {
+                viewModel.addAlbum(
+                    Album(
+                        "0",
+                        album_title.text.toString(),
+                        album_description.text.toString(),
+                        Calendar.getInstance()
+                    )
+                )
+            }
+        }
     }
 }
