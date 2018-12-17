20abe1e078b74f346b1a21898bfb26c3214c20c1
Evgeny Zinovyev <e.zinovyev@gmail.com>
2018-12-17 22:22:06 +0100

    bind recycleview to the collection fragment

--- app/src/main/java/com/ezino/digitalalbum/CollectionFragment.kt
+++ app/src/main/java/com/ezino/digitalalbum/CollectionFragment.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.ezino.digitalalbum.adapters.AlbumAdapter
+import com.ezino.digitalalbum.di.Injectors
+import com.ezino.digitalalbum.viewmodels.AlbumListViewModel
+import kotlinx.android.synthetic.main.content_album_collection.*
 
 /**
  * A simple [Fragment] subclass.
@@ -14,14 +19,23 @@ import androidx.fragment.app.Fragment
  */
 class CollectionFragment : Fragment() {
 
+    private lateinit var viewModel: AlbumListViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+        val factory = activity?.application?.let { Injectors.provideAlbumListViewModelFactory(it) }
+        viewModel = ViewModelProviders.of(this, factory).get(AlbumListViewModel::class.java)
+
+        return inflater.inflate(R.layout.content_album_collection, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val adapter = AlbumAdapter()
+        album_list.adapter = adapter
+        viewModel.getAlbums().observe(this, Observer { albums -> if (albums != null) adapter.submitList(albums) })
+    }
 }
